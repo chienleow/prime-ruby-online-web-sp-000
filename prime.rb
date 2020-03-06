b39ed890b3ad351a65b2a078842801d81f1aa3df
C. Leow <chienleow1@gmail.com>
2020-03-06 19:08:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,8 @@
-def prime?(integer)
-  prime.each(integer) do |prime|
-    prime
+def prime?(number)
+  if number <= 1
+    return false
+  else
+    (2..number-1).to_a.all? {|n| number % n != 0}
   end
 end
+puts prime?(254295849852984709857057)
